0786c91d6a6ae88096a38f7bbc9dfb80b28f652c
luooyii <luooyii@qq.com>
2025-08-28 10:01:37 +0800

    Fix deployment

--- monsoon-module-common/src/main/java/southwest/monsoon/module/common/mybatis/mapper/IdWithVersionControlMapper.java
+++ monsoon-module-common/src/main/java/southwest/monsoon/module/common/mybatis/mapper/IdWithVersionControlMapper.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import southwest.monsoon.module.common.mybatis.entity.IdWithVersionControl;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public interface IdWithVersionControlMapper<T extends IdWithVersionControl> extends BaseMapper<T> {
     default T getByPrimeKey(Object id, Integer version) {
@@ -19,8 +20,12 @@ public interface IdWithVersionControlMapper<T extends IdWithVersionControl> exte
     default T getLatestById(Object id) {
         LambdaQueryWrapper<T> wrapper = new LambdaQueryWrapper<>();
         wrapper.eq(T::getId, id);
+        wrapper.eq(T::getDeleted, false);
         wrapper.orderByDesc(T::getVersion);
-        return selectList(wrapper).getFirst();
+        for (T t : selectList(wrapper)) {
+            return t;
+        }
+        return null;
     }
 
     default int eraseActivationById(Timestamp now, String eraseBy, Object id) {
